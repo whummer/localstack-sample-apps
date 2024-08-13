7f62192efe130e450f4031d7a610e3a95d85735b
Waldemar Hummer <waldemar.hummer@gmail.com>
2024-08-13 16:09:58 +0200

    small fixes to get sample app working e2e

--- cockroachdb-snowflake-integration/main.tf
+++ cockroachdb-snowflake-integration/main.tf
@@ -1,57 +1,97 @@
-provider "aws" {
-  region     = "eu-central-1"
-  access_key = "fake"
-  secret_key = "fake"
+# provider "aws" {
+#   region     = "eu-central-1"
+#   access_key = "fake"
+#   secret_key = "fake"
+#
+#   skip_credentials_validation = true
+#   skip_metadata_api_check     = true
+#   skip_requesting_account_id  = true
+#   s3_use_path_style           = true
+#
+#   endpoints {
+#     s3  = "http://localhost:4566"
+#     iam = "http://localhost:4566"
+#   }
+#
+#   default_tags {
+#     tags = {
+#       Environment = "Local"
+#       Service     = "LocalStack"
+#     }
+#   }
+# }
 
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  s3_use_path_style           = true
+#
+# #############
+# # S3 Bucket #
+# #############
+# resource "aws_s3_bucket" "crdb_to_snowflake_demo" {
+#   bucket = "crdb-to-snowflake-cdc-demo"
+# }
+#
+# ########
+# # User #
+# ########
+# resource "aws_iam_user" "crdb_to_snowflake" {
+#   name = "crdb-to-snowflake"
+# }
+#
+# resource "aws_iam_user_policy" "crdb_to_snowflake" {
+#   name   = "crdb-to-snowflake"
+#   user   = aws_iam_user.crdb_to_snowflake.name
+#   policy = <<-EOF
+#   {
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Effect" : "Allow",
+#         "Action" : "*",
+#         "Resource" : "*"
+#       }
+#     ]
+#   }
+#   EOF
+# }
+#
+# resource "aws_iam_access_key" "crdb_to_snowflake" {
+#   user = aws_iam_user.crdb_to_snowflake.name
+# }
 
-  endpoints {
-    s3  = "http://localhost:4566"
-    iam = "http://localhost:4566"
-  }
-
-  default_tags {
-    tags = {
-      Environment = "Local"
-      Service     = "LocalStack"
-    }
-  }
-}
-
-#############
-# S3 Bucket #
-#############
 resource "aws_s3_bucket" "crdb_to_snowflake_demo" {
   bucket = "crdb-to-snowflake-cdc-demo"
 }
 
-########
-# User #
-########
-resource "aws_iam_user" "crdb_to_snowflake" {
-  name = "crdb-to-snowflake"
-}
+data "aws_iam_policy_document" "queue" {
+  statement {
+    effect = "Allow"
 
-resource "aws_iam_user_policy" "crdb_to_snowflake" {
-  name   = "crdb-to-snowflake"
-  user   = aws_iam_user.crdb_to_snowflake.name
-  policy = <<-EOF
-  {
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : "*",
-        "Resource" : "*"
-      }
-    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["sqs:SendMessage"]
+    resources = ["arn:aws:sqs:*:*:crdb-to-snowflake-notification-queue"]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.crdb_to_snowflake_demo.arn]
+    }
   }
-  EOF
 }
 
-resource "aws_iam_access_key" "crdb_to_snowflake" {
-  user = aws_iam_user.crdb_to_snowflake.name
+resource "aws_sqs_queue" "queue" {
+  # note: needs to be this particular name, to match the queue used internally in LocalStack Snowflake
+  name   = "sf-snowpipe-test"
+  policy = data.aws_iam_policy_document.queue.json
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.crdb_to_snowflake_demo.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.queue.arn
+    events        = ["s3:ObjectCreated:*"]
+  }
 }
--- cockroachdb-snowflake-integration/run.sh
+++ cockroachdb-snowflake-integration/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+tflocal init
+rm -f terraform.tfstate*
+tflocal apply -auto-approve
+
 snow_sql.sh "CREATE TABLE order_alerts (changefeed_record VARIANT)"
 
 snow_sql.sh "
 CREATE STAGE cdc_stage
-  ENDPOINT = 'http://localstack:4566'
+  ENDPOINT = 'http://localhost:4566'
   URL='s3compat://crdb-to-snowflake-cdc-demo'
   CREDENTIALS = (
     aws_key_id='local_stack_key_id'
@@ -24,15 +28,15 @@ CREATE PIPE cdc_pipe
   FILE_FORMAT = (TYPE = 'JSON')
 "
 
-awslocal s3 mb s3://crdb-to-snowflake-cdc-demo
+#awslocal s3 mb s3://crdb-to-snowflake-cdc-demo
 
 # add bucket notification, to inform Snowflake about new files on S3
 # see https://docs.snowflake.com/en/user-guide/data-load-snowpipe-auto-s3#determining-the-correct-option
-awslocal s3api put-bucket-notification-configuration --bucket crdb-to-snowflake-cdc-demo \
-  --notification-configuration '{
-    "QueueConfigurations": [{"Id": "c1", "QueueArn": "arn:aws:sqs:us-east-1:000000000000:sf-snowpipe-test", "Events": ["s3:ObjectCreated:*"]}],
-    "EventBridgeConfiguration": {}
-  }'
+#awslocal s3api put-bucket-notification-configuration --bucket crdb-to-snowflake-cdc-demo \
+#  --notification-configuration '{
+#    "QueueConfigurations": [{"Id": "c1", "QueueArn": "arn:aws:sqs:us-east-1:000000000000:sf-snowpipe-test", "Events": ["s3:ObjectCreated:*"]}],
+#    "EventBridgeConfiguration": {}
+#  }'
 
 awslocal s3 cp data.ndjson s3://crdb-to-snowflake-cdc-demo/
 
